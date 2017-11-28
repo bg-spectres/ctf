8e79a6ed70edf9cbf6617ec47beb0993d4b0adb3
chvrga <chvrga@gmail.com>
2017-11-28 10:27:41 +0100

    initial starter

---
+++ src/main/java/org/spectres/ctf/CtfApplication.java
@@ -0,0 +1,12 @@
+package org.spectres.ctf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtfApplication.class, args);
+	}
+}
