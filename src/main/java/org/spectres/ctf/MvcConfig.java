6713f1a503840c2ee96b5509c2e0c00f030860cf
chvrga <chvrga@gmail.com>
2017-12-01 16:17:11 +0100

    intial commit :)

---
+++ src/main/java/org/spectres/ctf/MvcConfig.java
@@ -0,0 +1,19 @@
+package org.spectres.ctf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/index").setViewName("index");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/error").setViewName("error");
+	}
+
+}
